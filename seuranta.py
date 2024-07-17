8a8365ebe44bb96ea6ffab9881c7586fc883a2ac
Alexander Goldhill <alejgold@jyu.fi>
2024-07-17 20:13:28 +0300

    refactor: use main entrypoint

--- seuranta.py
+++ seuranta.py
@@ -1,12 +1,18 @@
 import sqlite3
 
-with sqlite3.connect('seuranta.db') as conn:
-    cur = conn.cursor()
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS person (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE
-    )""")
+def main():
+    with sqlite3.connect('seuranta.db') as conn:
+        cur = conn.cursor()
+    
+        cur.execute("""CREATE TABLE IF NOT EXISTS person (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )""")
+    
+        cur.execute("INSERT OR IGNORE INTO person VALUES (?, ?)", (None, "45spoons"))
+        conn.commit()
 
-    cur.execute("INSERT OR IGNORE INTO person VALUES (?, ?)", (None, "45spoons"))
-    conn.commit()
+
+if __name__ == "__main__":
+    main()
