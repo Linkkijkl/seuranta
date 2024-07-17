0652aa941f6bf2956e6645d262e400c89e375bc1
Alexander Goldhill <alejgold@jyu.fi>
2024-07-18 01:11:10 +0300

    feat: implement db as own class, implement person addition

--- seuranta.py
+++ seuranta.py
@@ -1,17 +1,48 @@
 import sqlite3
 
 
-def main():
-    with sqlite3.connect('seuranta.db') as conn:
-        cur = conn.cursor()
-    
+class SeurantaDb(sqlite3.Connection):
+    def __init__(self, dbfile: str = "seuranta.db") -> None:
+        super().__init__(dbfile)
+        self._create_tables()
+
+
+    def _create_tables(self):
+        cur = self.cursor()
         cur.execute("""CREATE TABLE IF NOT EXISTS person (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE
-        )""")
-    
-        cur.execute("INSERT OR IGNORE INTO person VALUES (?, ?)", (None, "45spoons"))
-        conn.commit()
+        );""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS device (
+            person_id INTEGER PRIMARY KEY REFERENCES person (id),
+            macaddress TEXT UNIQUE
+        );""")
+
+
+    @property
+    def people(self) -> list[str]:
+        cur = self.cursor()
+        cur.execute("SELECT name FROM person")
+        return list(name for (name,) in cur.fetchall())
+
+
+    @property
+    def tables(self) -> list[str]:
+        cur = self.cursor()
+        cur.execute("SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%';")
+        return list(name for (name,) in cur.fetchall())
+
+
+    def addpeople(self, people: list[str]):
+        cur = self.cursor()
+        cur.executemany("INSERT OR IGNORE INTO person VALUES (?, ?)", ((None, person, ) for person in people))
+        self.commit()
+
+
+def main():
+    with SeurantaDb() as sdb:
+        sdb.addpeople(['45spoons'])
+        print(sdb.people)
 
 
 if __name__ == "__main__":
