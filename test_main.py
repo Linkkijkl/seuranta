8e70e15dbe5158625bab653bdc09837cfc4a56f1
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-07-29 02:30:42 +0300

    test: add simple test for index page working

---
+++ test_main.py
@@ -0,0 +1,17 @@
+import unittest
+from fastapi.testclient import TestClient
+from main import SeurantaApp
+
+
+class TestSeurantaApp(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = TestClient(SeurantaApp())
+
+
+    def test_index(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
