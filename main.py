ae2d93d92d94dfe8fe04ae5079c504b159729564
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-07-29 01:42:16 +0300

    feat: add templating and static files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import asyncio
 import uvicorn
 
@@ -8,11 +9,16 @@ class SeurantaUserInterface(FastAPI):
     def __init__(self):
         super().__init__()
         self.templates = Jinja2Templates(directory="templates")
+        self.mount("/static", StaticFiles(directory="static"), name="static")
         self.init_routes()
 
 
     def init_routes(self):
-        self.add_route("/", route=self.hello_kattila)
+        self.add_route("/", route=self.index)
+
+
+    async def index(self, req: Request) -> Response:
+        return self.templates.TemplateResponse(request=req, name="index.html")
 
 
     async def hello_kattila(self, _: Request) -> Response:
