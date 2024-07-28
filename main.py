e1617f268bdce371ee3758ab92843de87aecff08
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-07-29 00:58:46 +0300

    feat: add jinja2 templating

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.templating import Jinja2Templates
 import asyncio
 import uvicorn
 
@@ -6,6 +7,7 @@ import uvicorn
 class SeurantaUserInterface(FastAPI):
     def __init__(self):
         super().__init__()
+        self.templates = Jinja2Templates(directory="templates")
         self.init_routes()
 
 
