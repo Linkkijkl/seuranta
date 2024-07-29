ac26c55236b397e388ee450333f04d81277f862d
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-07-30 01:38:52 +0300

    feat: add scheduler stub

--- src/seuranta_app.py
+++ src/seuranta_app.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from apscheduler.schedulers.asyncio import AsyncIOScheduler # type: ignore
+from apscheduler.triggers.cron import CronTrigger # type: ignore
 
 
 class SeurantaApp(FastAPI):
     def __init__(self):
         super().__init__()
+
+        lease_monitor = AsyncIOScheduler()
+        lease_monitor.add_job(self.fetch_leases, CronTrigger(second="*/15")) # type: ignore
+        lease_monitor.start()
+
         self.templates = Jinja2Templates(directory="templates")
         self.mount("/static", StaticFiles(directory="static"), name="static")
         self.init_routes()
+
+
+    async def fetch_leases(self):
+        print("Retrieving DHCP leases... (NOT REALLY! Just pretending)")
 
 
     def init_routes(self):
