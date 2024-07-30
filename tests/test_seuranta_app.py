1979bcc9a9fe82cdabcc7d8e0fe569c27db6b361
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-07-31 00:26:25 +0300

    test: add lease parsing tests

--- tests/test_seuranta_app.py
+++ tests/test_seuranta_app.py
@@ -19,5 +19,24 @@ class TestSeurantaApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class TestSeurantaAppParsing(unittest.IsolatedAsyncioTestCase):
+    async def test_parse_leases_empty(self):
+        connected_clients = await SeurantaApp.parse_leases("")
+        self.assertListEqual([], connected_clients)
+
+
+    async def test_parse_leases_one_lease(self):
+        connected_clients = await SeurantaApp.parse_leases("0000000000 1a:2b:3c:4d:5e:6f 192.168.1.100 test-hostname 01:1a:2b:3c:4d:5e:6f")
+        self.assertListEqual([("1a:2b:3c:4d:5e:6f","192.168.1.100","test-hostname")], connected_clients)
+
+
+    async def test_parse_leases_two_leases(self):
+        lease_str = "0000000000 1a:2b:3c:4d:5e:6f 192.168.1.100 test-hostname-1 01:1a:2b:3c:4d:5e:6f\n1111111111 6f:5e:4d:3c:2b:1a 192.168.1.101 test-hostname-2 01:6f:5e:4d:3c:2b:1a"
+        ans = await SeurantaApp.parse_leases(lease_str)
+        exp = [("1a:2b:3c:4d:5e:6f","192.168.1.100","test-hostname-1"),
+               ("6f:5e:4d:3c:2b:1a","192.168.1.101","test-hostname-2")]
+        self.assertListEqual(exp, ans)
+
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
