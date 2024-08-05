a3efd497eb7d9fde447ceb09595f148fac8c2d81
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-08-05 23:23:12 +0300

    test: add database testing

---
+++ tests/test_seuranta_db.py
@@ -0,0 +1,54 @@
+import unittest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from src.seuranta_app import SeurantaApp, create_engine, get_session
+
+class TestSeurantaDb(unittest.TestCase):
+    def setUp(self) -> None:
+        engine = create_engine(
+            "sqlite://",
+            connect_args={"check_same_thread": False},
+            poolclass=StaticPool
+        )
+        SQLModel.metadata.create_all(engine)
+
+        with Session(engine) as session:
+            def get_session_override():
+                return session
+
+            testing_options = { "disable_lease_monitor": True,
+                                "disable_export": True}
+            with TestClient(app := SeurantaApp(**testing_options)) as client: # type: ignore
+                self.testclient = client
+                app.dependency_overrides[get_session] = get_session_override
+                self.app = app # needed for teardown, maybe fixtures would avoid this
+
+
+    def tearDown(self) -> None:
+        self.app.dependency_overrides.clear()
+
+
+    def test_create_trackedentity_empty_json(self):
+            response = self.testclient.post(
+                "/tracked", json={}
+            )
+
+            self.assertEqual(response.status_code, 422)
+
+
+    def test_create_trackedentity_valid_json(self):
+            response = self.testclient.post(
+                "/tracked", json={"name": "Alex"}
+            )
+            data = response.json()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data["name"], "Alex")
+            self.assertIsNotNone(data["id"])
+            self.assertIsNotNone(data["created_date"])
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
