9e10d0cb8efac64732e5c2dcef4120401353daa7
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-08-07 19:59:33 +0300

    test: add sanitisation tests

--- tests/test_seuranta_app.py
+++ tests/test_seuranta_app.py
@@ -1,4 +1,5 @@
 import unittest
+import string
 from fastapi.testclient import TestClient
 from src.seuranta_app import SeurantaApp, get_session
 from sqlmodel import Session, SQLModel, create_engine
@@ -37,6 +38,47 @@ class TestSeurantaApp(unittest.TestCase):
     def test_static_styles_exists(self):
         response = self.client.get("/static/styles.css")
         self.assertEqual(response.status_code, 200)
+
+
+    def test_name_form_minor_sanitisation(self):
+        form = {"name": "45_spoons"}
+        response = self.client.post(
+            "/name-form", data=form
+        )
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["name"], "45spoons")
+        self.assertIsNotNone(data["id"])
+        self.assertIsNotNone(data["created_date"])
+
+
+class TestNameSanitisation(unittest.IsolatedAsyncioTestCase):
+    async def test_trim_leading_whitespace(self):
+        result = await SeurantaApp.sanitise_name(" alex")
+        self.assertEqual(result, "alex")
+
+
+    async def test_trim_trailing_whitespace(self):
+        result = await SeurantaApp.sanitise_name("alex ")
+        self.assertEqual(result, "alex")
+
+
+    async def test_remove_bounded_whitespace(self):
+        result = await SeurantaApp.sanitise_name("al ex")
+        self.assertEqual(result, "alex")
+
+
+    async def test_valid_name(self):
+        result = await SeurantaApp.sanitise_name("AlexIs45spoons")
+        self.assertEqual(result, "AlexIs45spoons")
+
+
+    async def test_remove_punctuation_characters(self):
+        for char in string.punctuation:
+            with self.subTest(char=char):
+                result = await SeurantaApp.sanitise_name(f"al{char}ex")
+                self.assertEqual(result, "alex")
 
 
 if __name__ == "__main__":
