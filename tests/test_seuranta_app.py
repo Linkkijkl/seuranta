4a7858a4cbb55314e9791575b43090c8c9f073cc
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-08-07 19:58:07 +0300

    test: change setup for app tests

--- tests/test_seuranta_app.py
+++ tests/test_seuranta_app.py
@@ -1,6 +1,8 @@
 import unittest
 from fastapi.testclient import TestClient
-from src.seuranta_app import SeurantaApp
+from src.seuranta_app import SeurantaApp, get_session
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
 
 class TestSeurantaApp(unittest.TestCase):
@@ -11,9 +13,20 @@ class TestSeurantaApp(unittest.TestCase):
 
 
     def setUp(self) -> None:
-        with TestClient(app := SeurantaApp(**self.testing_options)) as client: # type: ignore
-            self.app = app
-            self.client = client
+        engine = create_engine(
+            "sqlite://",
+            connect_args={"check_same_thread": False},
+            poolclass=StaticPool
+        )
+        SQLModel.metadata.create_all(engine)
+
+        with Session(engine) as session:
+            def get_session_override():
+                return session
+            with TestClient(app := SeurantaApp(**self.testing_options)) as client: # type: ignore
+                self.app = app
+                self.client = client
+                app.dependency_overrides[get_session] = get_session_override
 
 
     def test_index_endpoint_exists(self):
