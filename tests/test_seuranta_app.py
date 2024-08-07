43fa70cacea78c2bc14ab071c37e86d206e390f2
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2024-08-08 01:39:21 +0300

    test: linking connected device to tracked entity

--- tests/test_seuranta_app.py
+++ tests/test_seuranta_app.py
@@ -1,16 +1,17 @@
 import unittest
+from unittest.mock import PropertyMock, patch
 import string
 from fastapi.testclient import TestClient
 from src.seuranta_app import SeurantaApp, get_session
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
+from src.lease_monitor import Lease
 
 
 class TestSeurantaApp(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.testing_options = { "disable_lease_monitor": True,
-                                "disable_export": True}
+        cls.testing_options = {  }
 
 
     def setUp(self) -> None:
@@ -43,7 +44,8 @@ class TestSeurantaApp(unittest.TestCase):
     def test_name_form_minor_sanitisation(self):
         form = {"name": "45_spoons"}
         response = self.client.post(
-            "/name-form", data=form
+            "/name-form",
+            data=form,
         )
         data = response.json()
 
@@ -51,6 +53,28 @@ class TestSeurantaApp(unittest.TestCase):
         self.assertEqual(data["name"], "45spoons")
         self.assertIsNotNone(data["id"])
         self.assertIsNotNone(data["created_date"])
+
+
+    def test_name_form_links_device(self):
+        with patch("src.seuranta_app.SeurantaApp.leases", new_callable=PropertyMock) as mock_leases:
+            # client host appears as "testclient" when posting from a testclient,
+            # as far as is known, there might be no way to alter this behaviour,
+            # setting Host headers does not change it.
+            mock_leases.return_value = [Lease("testclient","test-hostname-1","1a:2b:3c:4d:5e:6f"),
+                                        Lease("192.168.1.101","test-hostname-2","6f:5e:4d:3c:2b:1a")]
+            form = {"name": "45spoons"}
+            response = self.client.post(
+                "/name-form",
+                data=form,
+            )
+            data = response.json()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(data["name"], "45spoons")
+            self.assertIsNotNone(data["id"])
+            self.assertIsNotNone(data["created_date"])
+            self.assertEqual(data["devices"][0]["mac"], "1a:2b:3c:4d:5e:6f")
+            self.assertEqual(data["devices"][0]["trackedentity_id"], data["id"])
 
 
 class TestNameSanitisation(unittest.IsolatedAsyncioTestCase):
