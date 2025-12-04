af9815c3a082b17d8effc734f6e3505850484aec
Alexander Goldhill <73328090+45spoons@users.noreply.github.com>
2025-12-04 19:39:35 +0200

    fix: migration script no longer persists old ids

--- purkka.py
+++ purkka.py
@@ -11,14 +11,14 @@ PG_DBNAME = ""
 PG_PASSWD = ""
 DB_HOST = ""
 DB_URL = f"postgresql://{PG_USERNAME}:{PG_PASSWD}@{DB_HOST}/{PG_DBNAME}"
+OLD_DB = "seuranta.db"
 
 engine = create_engine(DB_URL)
+models.Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(engine)
 
-
-with sqlite3.connect("seuranta.db") as con:
-
-    with SessionLocal() as session:
+def migrate():
+    with sqlite3.connect(OLD_DB) as con, SessionLocal() as session:
         pgdevices = session.scalars(select(models.Device)).all()
         for device in pgdevices:
             print(device.mac_addr)
@@ -32,13 +32,11 @@ with sqlite3.connect("seuranta.db") as con:
         res = cur.execute("SELECT * FROM trackedentity")
         tracked_entities = {}
         while item := res.fetchone():
-            tracked_entity = models.TrackedEntity(id=item[1], name=item[0], created_datetime=datetime.datetime.fromisoformat(item[2]))
-            tracked_entities[item[1]] = tracked_entity
+            tracked_entities[item[1]] = models.TrackedEntity(name=item[0], created_datetime=datetime.datetime.fromisoformat(item[2]))
 
         res = cur.execute("SELECT * FROM device")
         while item := res.fetchone():
-            device = models.Device(id=item[3], mac_addr=item[1], hostname=item[0], tracked_entity_id=item[2])
-            tracked_entities[device.tracked_entity_id].devices.append(device)
+            tracked_entities[item[2]].devices.append(models.Device(mac_addr=item[1], hostname=item[0]))
 
         remove = []
         for (id, te) in tracked_entities.items():
@@ -50,5 +48,15 @@ with sqlite3.connect("seuranta.db") as con:
 
         for te in tracked_entities.values():
             print(te)
-        session.add_all(tracked_entities.values())
+            session.add(te)
+
         session.commit()
+
+def inspect():
+    with SessionLocal() as session:
+        pgtrackeds = session.scalars(select(models.TrackedEntity)).all()
+        for te in pgtrackeds:
+            print(te)
+
+migrate()
+inspect()
